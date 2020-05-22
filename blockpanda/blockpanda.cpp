d36c6b01aa4656b9bb902d4d099c1b22d132c191
Antony_eosDocker <a8252525@gmail.com>
2020-05-22 08:21:52 +0000

    Fail: Error 3015013: Unpack data exception

--- blockpanda/blockpanda.cpp
+++ blockpanda/blockpanda.cpp
@@ -9,16 +9,16 @@ public:
   blockpanda(name receiver, name code,  datastream<const char*> ds): contract(receiver, code, ds) {}
 
   [[eosio::action]]
-  void upsert(name user, std::string first_name) {
-    require_auth( user );
+  void c2r(name txid, name customer, std::string temp_cus) {
+    require_auth( customer );
     temp_index addresses( get_self(), get_first_receiver().value );
-    auto iterator = addresses.find(user.value);
+    auto iterator = addresses.find(txid.value);
     if( iterator == addresses.end() )
     {
-      addresses.emplace(user, [&]( auto& row ) {
-       row.key = user;
-       row.first_name = first_name;
-      //  row.last_name = last_name;
+      addresses.emplace(customer, [&]( auto& row ) {
+       row.txid = txid;
+       row.customer = customer;
+       row.temp_cus = temp_cus;
       //  row.street = street;
       //  row.city = city;
       //  row.state = state;
@@ -36,42 +36,42 @@ public:
   //   }
   }
 
-  [[eosio::action]]
-  void upsert2(name user, std::string last_name) {
-    require_auth( user );
-    temp_index addresses( get_self(), get_first_receiver().value );
-    auto iterator = addresses.find(user.value);
+  // [[eosio::action]]
+  // void upsert2(name txid, std::string last_name) {
+  //   require_auth( txid );
+  //   temp_index addresses( get_self(), get_first_receiver().value );
+  //   auto iterator = addresses.find(txid.value);
 
-      addresses.modify(iterator, user, [&]( auto& row ) {
-       row.key = user;
-       row.last_name = last_name;
-      //  row.last_
-      //  row.stree
-      //  row.city
-      //  row.state
-      });
-  }
+  //     addresses.modify(iterator, txid, [&]( auto& row ) {
+  //      row.txid = txid;
+  //      row.last_name = last_name;
+  //     //  row.last_
+  //     //  row.stree
+  //     //  row.city
+  //     //  row.state
+  //     });
+  // }
   
   [[eosio::action]]
-  void erase(name user) {
-    require_auth(user);
+  void erase(name txid) {
+    //require_auth(txid);
 
     temp_index addresses( get_self(), get_first_receiver().value);
 
-    auto iterator = addresses.find(user.value);
+    auto iterator = addresses.find(txid.value);
     check(iterator != addresses.end(), "Record does not exist");
     addresses.erase(iterator);
   }
 
 private:
   struct [[eosio::table]] person {
-    name key;
-    std::string first_name;
-    std::string last_name;
+    name txid;
+    name customer;
+    std::string temp_cus;
     // std::string street;
     // std::string city;
     // std::string state;
-    uint64_t primary_key() const { return key.value; }
+    uint64_t primary_key() const { return txid.value; }
   };
   typedef eosio::multi_index<"people"_n, person> temp_index;
 
--- blockpanda/panda.sh
+++ blockpanda/panda.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
+
 cleos wallet unlock --password PW5JAAqDnfrN56bHxsYeci94RSwARcqrLeSUimvKFmrejRw5n13Lm
 eosio-cpp blockpanda.cpp -o blockpanda.wasm
 cleos set contract blockpanda ../blockpanda/ -p blockpanda@active
-cleos push action blockpanda upsert '["bob", "bob"]' -p bob@active
-cleos push action blockpanda upsert2 '["bob", "pppppanda"]' -p bob@active
+echo "cleos push action blockpanda upsert "
+cleos push action blockpanda c2r '["123123", "bob", ""]' -p bob@active
+#cleos push action blockpanda upsert2 '["bob", "no panda here"]' -p bob@active
 cleos get table blockpanda blockpanda people
+
+#cleos push action addressbook erase '["123123"]' -p bob@active
