c1b5db2ac30982f5e1eb0aab3ef3e7e11cdfe4ac
tony <tony92151@gmail.com>
2020-07-10 11:00:11 +0800

    dfuse update

---
+++ dfuse_implement/launch_local_testnet.sh
@@ -0,0 +1,9 @@
+set_local_testnet(){
+    cd
+    git clone https://github.com/Intelligent-Systems-Lab/EOS-lab-testnet.git
+    echo -ne '\n' | bash EOS-lab-testnet/quick_start_multi.sh
+}
+
+
+read -p "Press [Enter] set start local testnet..."
+set_local_testnet
